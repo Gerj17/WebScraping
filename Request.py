0ebd01a4f6fd4a4401d4e605ad4eb812f186edf0
Gerard <jonesgerard25@gmail.com>
2018-02-14 21:49:40 -0400

    later considerations

--- Request.py
+++ Request.py
@@ -1,10 +1,15 @@
 
-def request_anime ():
+
+# The first page of the GUI
+
+def request_anime(finished): # this will be integrated with tikinter
     anime_request = []
     while True:
         requests = input('Enter the anime you would like to know about')
         anime_request.append(requests)
+        if finished:
+            return anime_request
 
 
 
-# more woke to be done
+
