64a34737697d9291d6a4be4b83f40c4117ccec36
Gerard <jonesgerard25@gmail.com>
2018-02-17 23:40:10 -0400

    Tkinter

--- interface.py
+++ interface.py
@@ -1,3 +1,25 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
 
-tkinter._test()
+root = Tk()
+
+root.title("Name of application")
+
+frame = Frame(root)
+
+Label(frame, text="Your anime").grid(row=0, sticky=W, padx=4)
+Entry(frame,width=50).grid(row=0, column=1,pady=4)
+
+Button(frame, text="Submit").grid(row=0, column=8)
+
+
+
+
+
+
+
+
+
+frame.grid()
+
+root.mainloop()
