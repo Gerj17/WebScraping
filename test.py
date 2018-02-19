54c432f5b9e0b304faa6cdfef40e9ce45da3250c
Gerard <jonesgerard25@gmail.com>
2018-02-18 22:48:38 -0400

    Tkinter

---
+++ test.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.title("Name of application")
+
+frame1 = Frame(root, width=500, height=250)
+frame1.grid()
+
+
+anime = Button(frame1,text="anime")
+anime.grid()
+
+
+
+root.mainloop()
+
