fa364e1af143f15595123c15ea72b64d403bf3f0
Gerard <jonesgerard25@gmail.com>
2018-02-18 20:06:17 -0400

    Tkinter

--- interface.py
+++ interface.py
@@ -1,16 +1,9 @@
 from tkinter import *
-from tkinter import ttk
 
 root = Tk()
 
 root.title("Name of application")
 
-frame = Frame(root)
-
-Label(frame, text="Your anime").grid(row=0, sticky=W, padx=4)
-Entry(frame,width=50).grid(row=0, column=1,pady=4)
-
-Button(frame, text="Submit").grid(row=0, column=8)
 
 
 
@@ -20,6 +13,5 @@ Button(frame, text="Submit").grid(row=0, column=8)
 
 
 
-frame.grid()
 
 root.mainloop()
