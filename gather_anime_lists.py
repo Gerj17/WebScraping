87f10776192741dcde8d31dbc49ea61de239385f
Gerard <jonesgerard25@gmail.com>
2018-04-06 06:59:24 -0400

    Finished youranime section of GUI
    Took 21 minutes

--- gather_anime_lists.py
+++ gather_anime_lists.py
@@ -7,7 +7,7 @@ def gather_gogo_anime():
     from gapurl import parseurl
     """adds anime link, plot,genres and release date to dictionary name anime with the key being the anime name
     """
-
+    all_anime = load(open("anime_save.p", "rb"))
     gogo = 'http://www3.gogoanime.tv/'
     gogo_anime_list = 'http://www3.gogoanime.tv/anime-list-0?page=1'
 
@@ -26,48 +26,50 @@ def gather_gogo_anime():
         for li in listing:
             # get title of anime
             title = li.a.text
-
             # get link to anime has to concatenated with gogo (loops once)
             for url in li.find_all('a', href=True):
                 link = gogo + (url['href'])
 
                 if title in all_anime:
+                    print("\n", title, "\n")
                     continue
-
-                print('Second Parsing')
-                anime_soup = parseurl(link)
-                print('Done Second Parsing\n')
-                anime_info = anime_soup.find("div", {"class", "anime_info_body_bg"}).findAll("p")
-
-                plot = anime_info[2].text
-
-                gets_genre = anime_info[3].findAll('a')
-                genre = []
-                for x in gets_genre:
-                    genre.append(x['title'])
-
-                release_date = anime_info[4].text[9:]
-
-                """
-                # may add if useful Status
-                status = anime_info[5].text[8:]
-
-                latest_episodes = anime_soup.find("div", {"class", "anime_video_body"})
-
-                if status == "Completed":
-                    episodes = latest_episodes.ul.li.a.text[2:]
-                    all_anime[title] = episodes
                 else:
-                    current_episode = latest_episodes.ul.li.a.text[2:]
-                    all_anime[title] = current_episode
+                    print("\n", title, "\n")
+
+                    print('Second Parsing')
+                    anime_soup = parseurl(link)
+                    print('Done Second Parsing\n')
+                    anime_info = anime_soup.find("div", {"class", "anime_info_body_bg"}).findAll("p")
+
+                    plot = anime_info[2].text
+
+                    gets_genre = anime_info[3].findAll('a')
+                    genre = []
+                    for x in gets_genre:
+                        genre.append(x['title'])
+
+                    release_date = anime_info[4].text[9:]
+
                     """
+                    # may add if useful Status
+                    status = anime_info[5].text[8:]
+    
+                    latest_episodes = anime_soup.find("div", {"class", "anime_video_body"})
+    
+                    if status == "Completed":
+                        episodes = latest_episodes.ul.li.a.text[2:]
+                        all_anime[title] = episodes
+                    else:
+                        current_episode = latest_episodes.ul.li.a.text[2:]
+                        all_anime[title] = current_episode
+                        """
 
-                all_anime[title] = plot, genre, release_date  # Final product
-                dump(all_anime, open("anime_save.p", "wb"))  # store all anime in file
+                    all_anime[title] = plot, genre, release_date  # Final product
+                    dump(all_anime, open("anime_save.p", "wb"))  # store all anime in file
 
-                """consider that the website knows how many episodes a show should have and gogo has current episodes 
-                probably probably can use that with an if statement to give 'current episode', 'expected episode' and 
-                'episodes remaining' """  # Code for consideration
+                    """consider that the website knows how many episodes a show should have and gogo has current episodes 
+                    probably probably can use that with an if statement to give 'current episode', 'expected episode' and 
+                    'episodes remaining' """  # Code for consideration
 
         # Breaks loop, if there is no list oof anime in the url it will break
         if not listing:
@@ -90,9 +92,10 @@ def gather_gogo_anime():
 
 
 all_anime = {}
+
+
 gather_gogo_anime()
 
 
 # dump(all_anime, open("testing_save_.p", "wb"))  # store anime in file
 dump(all_anime, open("anime_save.p", "wb"))  # store all anime in file
-all_anime = load(open("anime_save.p", "rb"))
